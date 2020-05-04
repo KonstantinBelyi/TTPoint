f1aae570ec44fd6fcf1127e34c987feb08358bcb
Dmitry Filonenko <fildv7@gmail.com>
2020-05-04 23:06:12 +0300

    Player model has been added.

---
+++ TTPoint/TTPoint/Models/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TTPoint.Models
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public string WinCount { get; set; }
+    }
+}
