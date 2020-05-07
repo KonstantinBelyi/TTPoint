0c25ee25fceaf7d1227b7d4537fe132b9f21944f
Dmitry Filonenko <fildv7@gmail.com>
2020-05-07 21:18:50 +0300

    The PlayerViewModel has been created.

---
+++ TTPoint/TTPoint/ViewModels/PlayerViewModel.cs
@@ -0,0 +1,61 @@
+﻿
+using System.ComponentModel;
+using TTPoint.Models.Player;
+
+namespace TTPoint.ViewModels
+{
+    class PlayerViewModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public TtPlayer Player { get; private set; }
+
+        public PlayerViewModel()
+        {
+            Player = new TtPlayer();
+        }
+
+        public string Name
+        {
+            get { return Player.Name; }
+            set
+            {
+                if (Player.Name != value)
+                {
+                    Player.Name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }        
+       
+        public bool IsValid
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(Name.Trim());
+            }
+        }
+
+        protected void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public void AddWin()
+        {
+            Player.WinCount++;
+        }
+
+        public void AddLoss()
+        {
+            Player.LossCount++;
+        }
+
+        public bool SavePlayerData()
+        {
+            // try to save player data to file
+            return true;
+        }
+    }
+}
