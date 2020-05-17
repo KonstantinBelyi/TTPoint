b6fa7e6272abec4e4f8be46f322a2985bd29b302
Konstantin <kos.bel2@gmail.com>
2020-05-17 21:05:17 +0300

    added pages and PlayersListViewModel class

---
+++ TTPoint/TTPoint/ViewModels/PlayersListViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using TTPoint.Views;
+using Xamarin.Forms;
+
+namespace TTPoint.ViewModels
+{
+    public class PlayersListViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<PlayerViewModel> Players { get; set; }
+
+        public ICommand CreatePlayerCommand { get; protected set; }
+        public ICommand DeletePlayerCommand { get; protected set; }
+        public ICommand SavePlayerCommand { get; protected set; }
+        public ICommand BackCommand { get; protected set; }
+        PlayerViewModel selectedFriend;
+
+        public INavigation Navigation { get; set; }
+
+        public PlayersListViewModel()
+        {
+            Players = new ObservableCollection<PlayerViewModel>();
+            CreatePlayerCommand = new Command(CreatePlayer);
+            DeletePlayerCommand = new Command(DeletePlayer);
+            SavePlayerCommand = new Command(SavePlayer);
+            BackCommand = new Command(Back);
+        }
+
+        private void CreatePlayer()
+        {
+            Navigation.PushAsync(new PlayerPage(new PlayerViewModel() { ListViewModel = this }));
+        }
+
+        private void Back()
+        {
+            Navigation.PopAsync();
+        }
+
+        private void SavePlayer(object playerObj)
+        {
+            PlayerViewModel player = playerObj as PlayerViewModel;
+            if (player != null && player.IsValid)
+            {
+                Players.Add(player);
+            }
+            Back();
+        }
+
+        private void DeletePlayer(object playerObj)
+        {
+            PlayerViewModel player = playerObj as PlayerViewModel;
+            if (player != null)
+            {
+                Players.Remove(player);
+            }
+            Back();
+        }
+
+        
+
+        public PlayerViewModel SelectedFriend
+        {
+            get { return selectedFriend; }
+            set 
+            {
+                if (selectedFriend != null)
+                {
+                    PlayerViewModel playerViewModel = value;
+                    selectedFriend = null;
+                    OnPropertyChanged("SelectedFriend");
+                    Navigation.PushAsync(new PlayerPage(playerViewModel));
+                }
+            }
+        }
+
+        protected void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
---
+++ TTPoint/TTPoint/Views/PlayerPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TTPoint.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TTPoint.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PlayerPage : ContentPage
+    {
+        public PlayerViewModel ViewModel { get; private set; }
+        public PlayerPage(PlayerViewModel viewModel)
+        {
+            InitializeComponent();
+            ViewModel = viewModel;
+            BindingContext = ViewModel;
+        }
+    }
+}
